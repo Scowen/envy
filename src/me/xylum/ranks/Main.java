fbf99137a67be6db71058d6f9fa4018878320fd7
Xylum <lmscowen@gmail.com>
2018-07-24 09:57:39 +0100

    Init

---
+++ src/me/xylum/ranks/Main.java
@@ -0,0 +1,15 @@
+package me.xylum.ranks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
